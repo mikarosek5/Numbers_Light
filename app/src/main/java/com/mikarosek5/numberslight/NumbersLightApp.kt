665838c1dd13d3e567640a2cdc479f0de0a76971
mateusz <mateusz.kolbusz.poczta@gmail.com>
2022-10-14 13:22:50 +0200

    Git init

---
+++ app/src/main/java/com/mikarosek5/numberslight/NumbersLightApp.kt
@@ -0,0 +1,10 @@
+package com.mikarosek5.numberslight
+
+import android.app.Application
+import dagger.hilt.EntryPoint
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class NumbersLightApp : Application() {
+
+}

9f424d57df6141e177cf463314ef7300cb379051
mateusz <mateusz.kolbusz.poczta@gmail.com>
2022-10-26 15:45:38 +0200

    Details refactored to android compose, Code cleanup now: RxJava Observables are collected as state and automatically disposed after screen disposes.

--- app/src/main/java/com/mikarosek5/numberslight/feature_numbers_and_images/presentation/numbers_light_details/NumberLightDetailFragment.kt
+++ app/src/main/java/com/mikarosek5/numberslight/feature_numbers_and_images/presentation/numbers_light_details/NumberLightDetailFragment.kt
@@ -5,22 +5,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.rxjava3.subscribeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.fragment.app.Fragment
-import com.mikarosek5.numberslight.R
+import com.mikarosek5.numberslight.feature_numbers_and_images.domain.model.NumberLightDetail
+import com.mikarosek5.numberslight.feature_numbers_and_images.presentation.theme.TheNumbersLightTheme
 import com.squareup.picasso.Picasso
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
@@ -32,68 +31,63 @@ class NumberLightDetailFragment : Fragment() {
     @Inject
     lateinit var viewModel: NumberLightDetailsViewModel
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        this.arguments?.let {
-            val id = it.getString("id")
-            if (id != null) {
-                viewModel.getDetail(id)
-            } else {
-                Toast.makeText(this.context, "bundle is empty", Toast.LENGTH_LONG).show()
-            }
+    private lateinit var composeView: ComposeView
 
-
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return ComposeView(requireContext()).also {
+            composeView = it
         }
     }
 
-    override fun onResume() {
-        super.onResume()
-        view?.apply {
-            findViewById<ComposeView>(R.id.composeView).apply {
-                setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
-                setContent {
-                    viewModel.detail.observeAsState().value?.let {
-                        Column(
-                            horizontalAlignment = Alignment.CenterHorizontally,
-                            verticalArrangement = Arrangement.SpaceAround
-                        ) {
-                            Text(text = it.id, modifier = Modifier.padding(horizontal = 8.dp))
-                            Column(
-                                horizontalAlignment = Alignment.CenterHorizontally,
-                                verticalArrangement = Arrangement.SpaceAround
-                            ) {
-                                Text(
-                                    text = it.word,
-                                    fontSize = 50.sp,
-                                    modifier = Modifier.padding(horizontal = 8.dp)
-                                )
-                                AndroidView(factory = { ImageView(it) }) { imageView ->
-                                    imageView.apply {
-                                        Picasso.get().load(it.imageUrl).into(this)
-                                        minimumWidth = 500
-                                        minimumHeight = 500
-                                    }
-                                }
-                            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        composeView.setContent {
+            TheNumbersLightTheme {
+                this.arguments?.getString("id")?.let {
+                    viewModel
+                        .getObservableDetail(it)
+                        .subscribeAsState(initial = null)
+                        .value
+                        ?.let { details->
+                            DetailsScreen(detail = details)
                         }
-                    }
+                }
+            }
+        }
+    }
+
+
+
+}
+
+@Composable
+fun DetailsScreen(detail:NumberLightDetail) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceAround
+    ) {
+        Text(text = detail.id, modifier = Modifier.padding(horizontal = 8.dp))
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceAround
+        ) {
+            Text(
+                text = detail.word,
+                fontSize = 50.sp,
+                modifier = Modifier.padding(horizontal = 8.dp)
+            )
+            AndroidView(factory = { ImageView(it) }) { imageView ->
+                imageView.apply {
+                    Picasso.get().load(detail.imageUrl).into(this)
+                    minimumWidth = 500
+                    minimumHeight = 500
                 }
             }
         }
 
     }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_number_light_detail, container, false)
-    }
-
-}
-
-@Composable
-fun DetailsScreen() {
-
 }
--- app/src/main/java/com/mikarosek5/numberslight/feature_numbers_and_images/presentation/numbers_light_details/NumberLightDetailsViewModel.kt
+++ app/src/main/java/com/mikarosek5/numberslight/feature_numbers_and_images/presentation/numbers_light_details/NumberLightDetailsViewModel.kt
@@ -1,32 +1,11 @@
 package com.mikarosek5.numberslight.feature_numbers_and_images.presentation.numbers_light_details
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.mikarosek5.numberslight.feature_numbers_and_images.domain.model.NumberLightDetail
 import com.mikarosek5.numberslight.feature_numbers_and_images.domain.use_cases.GetNumberLightDetailsUseCase
-import io.reactivex.rxjava3.disposables.CompositeDisposable
 import javax.inject.Inject
 
 class NumberLightDetailsViewModel @Inject constructor(
     private val numberDetails: GetNumberLightDetailsUseCase
 ) : ViewModel() {
-
-    private val disposable = CompositeDisposable()
-    private val _detail = MutableLiveData<NumberLightDetail>()
-    val detail: LiveData<NumberLightDetail>
-        get() = _detail
-
-
-    fun getDetail(id: String) {
-        disposable.add(numberDetails(id).subscribe (
-            {onConsume-> _detail.postValue(onConsume)},
-            {onError->  }
-        ))
-    }
-
-    override fun onCleared() {
-        super.onCleared()
-        disposable.clear()
-    }
+    fun getObservableDetail(id: String) = numberDetails(id)
 }
